dffb228684daf1611f791c8a38e3eda29ecd39d8
mogtaba <e_mogtaba@outlook.com>
2024-05-11 10:47:38 +0300

    Add login.php

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <title>تسجيل الدخول</title>
+</head>
+<body>
+    <h1>تسجيل الدخول</h1>
+    <form method="post" action="insert_data.php">
+        <label for="username">اسم المستخدم:</label>
+        <input type="text" id="username" name="username">
+        <label for="password">كلمة المرور:</label>
+        <input type="password" id="password" name="password">
+        <input type="submit" value="تسجيل الدخول">
+    </form>
+</body>
+</html>
