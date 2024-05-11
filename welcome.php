46abc664cca9289f2c9b3c950084350efade0f1d
mogtaba <e_mogtaba@outlook.com>
2024-05-11 10:30:44 +0300

    أضافة welcome.php

---
+++ welcome.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <title>صفحة الرئيسية</title>
+</head>
+<body>
+    <h1>مرحبا بك في صفحة الرئيسية</h1>
+    <a href="login.php">تسجيل الدخول</a>
+</body>
+</html>
